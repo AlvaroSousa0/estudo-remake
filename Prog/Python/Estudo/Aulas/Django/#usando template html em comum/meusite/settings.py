a8ab9c3d0ed4e38bb061ef46d5cae2262eccd9bb
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2024-05-16 08:39:13 -0300

    ..

--- Prog/Python/Estudo/Aulas/Django/#usando template html em comum/meusite/settings.py
+++ Prog/Python/Estudo/Aulas/Django/#usando template html em comum/meusite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fnl$^p51cpl6mwge=-9l4#py3j=epzv_@7u*$12s$6uk^^4%+$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
