6c8cd6bf60d835917db2fa8b715d0c9123c30a95
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2024-06-12 14:11:17 -0300

    ..

---
+++ Prog/Java/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Comentário JavaDoc
+ */
+public class Aula01OlaDevDojo {
+    //Isso é um comentário de uma linha
+
+    /*
+        comentaário
+        multiplas linhas
+        .
+     */
+
+
+    public static void main(String[] args){
+        System.out.println("Olá mundo");
+    }
+}
---
+++ Prog/Java/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main (String[] args){
+        //int, double, float, char, byte, short, long, boolean
+        int idadeDoPai = 45;
+                System.out.println("A idade do pai é: " + idadeDoPai);
+    }
+}
