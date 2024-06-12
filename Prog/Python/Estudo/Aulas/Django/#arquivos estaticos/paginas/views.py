abb56ebf5371d12a8600e7064ffb6d82c533ce6b
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2024-06-12 13:45:10 -0300

    ....

--- Prog/Python/Estudo/Aulas/Django/#arquivos estaticos/paginas/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    return render(request, 'paginas/index.html')
-
-def sobred(request):
-    return render(request, 'sobre/index.html')
