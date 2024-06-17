ae6f65116d52be9b8fdad92afa3649109d867e3d
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2024-06-17 10:07:46 -0300

    doc

--- Prog/Java/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ Prog/Java/maratona-java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,6 +4,14 @@ public class Aula02TiposPrimitivos {
     public static void main (String[] args){
         //int, double, float, char, byte, short, long, boolean
         int idadeDoPai = 45;
+        long salarioLong = 200000;
+        double salarioDouble = 2000;
+        float salarioFLoat = 2500;
+        byte idadeByte = 14;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean mentira = false;
+        char caractere = 'M';
                 System.out.println("A idade do pai é: " + idadeDoPai);
     }
 }
