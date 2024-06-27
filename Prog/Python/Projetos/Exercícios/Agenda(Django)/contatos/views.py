9ce303a064406eb43391d7b5efde48c6769591aa
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2024-06-27 09:14:33 -0300

    ..

--- Prog/Python/Projetos/Exercícios/Agenda(Django)/contatos/views.py
+++ Prog/Python/Projetos/Exercícios/Agenda(Django)/contatos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from django.core.paginator import Paginator
 from .models import Contato
 
@@ -16,8 +17,10 @@ def index(request):
         })
 
 def ver_contato(request, contato_id):
-    # contato = Contato.objects.get(id=contato_id)
+
     contato = get_object_or_404(Contato, id=contato_id)
+    if not contato.mostrar:
+        raise Http404()
     return render(request,'contatos/ver_contato.html', {
         'contato': contato
     })
