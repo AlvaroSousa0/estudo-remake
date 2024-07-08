e14e38f846457ea9b0c22f89c7b520050f23fdbf
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2024-07-08 06:52:58 -0300

    ..

---
+++ Prog/Python/Estudo/Aulas/POO/Classe/main.py
@@ -0,0 +1,16 @@
+class Pessoa:
+    def __init__(self, nome, sobrenome):
+        self.nome = nome
+        self.sobrenome = sobrenome
+
+
+
+
+
+p1 = Pessoa("João", "Neto")
+print(p1.nome)
+print(p1.sobrenome)
+
+class Carro:
+    def __init__(self):
+        ...
--- Prog/Python/Projetos/Os Próprios/Gerador de Senhas/main.py
+++ Prog/Python/Projetos/Os Próprios/Gerador de Senhas/main.py
@@ -6,10 +6,10 @@ while True:
     senha = ''
 
     while len(senha) < qtd_caracteres:
-        letra_numero_especial = random.randint(0,2)
-        if letra_numero_especial == 0:
+        letra_numero_ou_especial = random.randint(0,2)
+        if letra_numero_ou_especial == 0:
             senha += random.choice(string.ascii_letters)
-        elif letra_numero_especial == 1:
+        elif letra_numero_ou_especial == 1:
             caracteres_especiais = '!@#$%&*-+=~^?/:;.,<>'
             escolha_caractere = random.randint(0,19)
             senha += caracteres_especiais[escolha_caractere]
