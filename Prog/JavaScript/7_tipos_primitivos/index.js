b4924537182c482a92cc1a25c2a09c0776a53611
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2024-07-29 10:43:27 -0300

    .

---
+++ Prog/JavaScript/7_tipos_primitivos/index.js
@@ -0,0 +1,8 @@
+const nome = 'João';// string
+const nome1 = "José";// string
+const nome2 = `Julio`;// string
+const num1 = 30;// number
+const num2 = 10.56;// number
+let nomeAluno;// undefined
+const sobrenomeAluno = null;// null -> Não aponta para local nenhum na memoria
+const aprovado = false;// boolean true , false
---
+++ Prog/JavaScript/8_operadores_aritmeticos/index.js
@@ -0,0 +1,22 @@
+/** 
+ * Aritméticos
+ * +  Adição / Concatenação
+ * ++ Incremento
+ * -  Subtração
+ * -- Decremento
+ * *  Multiplicação
+ * /  Divisão
+ * ** Potenciação
+ * %  Resto de divisão / Módulo
+ * 
+*/
+
+const num1 = 10;
+const num2 = 3;
+
+console.log( num1 + num2 );
+console.log( num1 - num2 );
+console.log( num1 * num2 );
+console.log( num1 / num2 );
+console.log( num1 ** num2 );
+console.log( num1 % num2 );
