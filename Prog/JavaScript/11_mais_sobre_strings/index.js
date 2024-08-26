8041ef8a6d4b4595e673c8caa0bc7957e7ecc547
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2024-08-26 11:28:05 -0300

    Create index.js

---
+++ Prog/JavaScript/11_mais_sobre_strings/index.js
@@ -0,0 +1,15 @@
+let string = "\"É uma string\\\"";
+
+console.log(string);
+
+console.log(string[13]);
+
+console.log(string.concat(' somente isso'));
+console.log(string,'somente isso');
+console.log(`${string} somente isso`);
+
+console.log(string.indexOf('\\', 10));
+
+console.log(string.match(/[a-z]/g));
+
+console.log(string.replace('uma', 'outra'));
