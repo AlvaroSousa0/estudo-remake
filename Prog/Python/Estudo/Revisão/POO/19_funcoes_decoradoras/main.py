36890e5a9b2ec995c4411954458fa7db25670082
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2024-09-02 16:10:14 -0300

    Update main.py

--- Prog/Python/Estudo/Revisão/POO/19_funcoes_decoradoras/main.py
+++ Prog/Python/Estudo/Revisão/POO/19_funcoes_decoradoras/main.py
@@ -8,26 +8,40 @@ def add_repr(cls):
     return cls
 
 
-@add_repr
+def meu_planeta(metodo):
+    def interno(self, *args, **kwargs):
+        resultado = metodo(self, *args, **kwargs)
+
+        if 'Terra' in resultado:
+            return 'Voce está em casa.'
+        return resultado
+    return interno
+
+
+# @add_repr
 class Time:
     def __init__(self, nome):
         self.nome = nome
 
 
-@add_repr
+# @add_repr
 class Planeta:
     def __init__(self, nome):
         self.nome = nome
+    
+    @meu_planeta
+    def falar_nome(self):
+        return f'O planeta é {self.nome}'
 
 
-time1 = Time('Vasco')
-time2 = Time('Fluminense')
+# time1 = Time('Vasco')
+# time2 = Time('Fluminense')
 
 planeta = Planeta('Terra')
 planeta2 = Planeta('Venus')
 
-print(time1)
-print(time2)
-print()
-print(planeta)
-print(planeta2)
+# print(time1)
+# print(time2)
+# print()
+print(planeta.falar_nome())
+print(planeta2.falar_nome())
