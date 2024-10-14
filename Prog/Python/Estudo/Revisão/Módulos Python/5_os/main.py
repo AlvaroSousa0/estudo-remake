d5cbdf152c15266e9f09f7e4caa26fd45f45fa61
Alvaro <149611507+AlvaroSousa0@users.noreply.github.com>
2024-10-14 14:30:47 -0300

    Update main.py

--- Prog/Python/Estudo/Revisão/Módulos Python/5_os/main.py
+++ Prog/Python/Estudo/Revisão/Módulos Python/5_os/main.py
@@ -5,13 +5,22 @@ import os
 # os.system('echo "Hello world"')
 
 
-# print('a'*800)
+# # print('a'*800)
 
-caminho = os.path.join('/home', 'alvaro', 'Documentos', 'GitHub', 'estudo-remake', 'Prog', 'Python', 'main.py')
-# print(caminho)
-diretorio, arquivo = os.path.split(caminho)
-# print(diretorio)
-# print(arquivo)
-nome_arquivo, extensao_arquivo = os.path.splitext(arquivo)
-# print(nome_arquivo, extensao_arquivo)
-print(os.path.exists(caminho))
+# caminho = os.path.join('/home', 'alvaro', 'Documentos', 'GitHub', 'estudo-remake', 'Prog', 'Python', 'main.py')
+# # print(caminho)
+# diretorio, arquivo = os.path.split(caminho)
+# # print(diretorio)
+# # print(arquivo)
+# nome_arquivo, extensao_arquivo = os.path.splitext(arquivo)
+# # print(nome_arquivo, extensao_arquivo)
+# print(os.path.exists(caminho))
+caminho = os.path.join('/home', 'alvaro', 'Imagens')
+
+for item in os.listdir(caminho):
+    
+    caminho_completo = os.path.join(caminho, item)
+    print(item)
+
+    for imagem in os.listdir(caminho_completo):
+        print(' ', imagem)
